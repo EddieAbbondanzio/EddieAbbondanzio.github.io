2fbfd02c45be49843104bf538930cdc5ca470c65
Ed Abbondanzio <me@eddieabbondanz.io>
2024-12-11 22:53:31 -0500

    24-12-11

--- js/lol.js
+++ js/lol.js
@@ -13,3 +13,17 @@ if (hamburger && hamburgerMenu) {
     }
   });
 }
+
+const navbarDropdowns = document.querySelectorAll(".item.dropdown");
+for (const nb of navbarDropdowns) {
+  nb.addEventListener("pointerup", ev => {
+    // Ensure we aren't blocking taps on items.
+    if(ev.target.closest(".content")) {
+      return
+    }
+    
+    if (ev.pointerType === "touch") {
+      nb.classList.toggle("active");
+    } 
+  });
+}
